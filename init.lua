c47aaa65914fa9b8030d33792d173465fbc3bfd7
Bitslayn <bitslayn@outlook.com>
2026-06-22 06:36:11 -0400

    Update chess

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
 local boards = require("./comp/boards")
 local hitbox = require("./comp/hitbox")
+local timers = require("./comp/timers")
 
-function events.entity_init()
-	local board = boards.new()
-	hitbox.new(player, board)
-end
+-- function events.entity_init()
+-- 	-- local board = boards.new()
+-- 	-- hitbox.new(player, board)
+-- 	local black = timers.new(player, "Black", 10)
+-- 	function events.tick()
+-- 		black:tick()
+-- 	end
+-- end
